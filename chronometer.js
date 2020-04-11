6736416198b1046e891490537b8b89d8f72ad082
mrrezaahmadi <mrrezaahmadi>
2020-04-11 17:35:44 +0430

    add reset button functionality

--- chronometer.js
+++ chronometer.js
@@ -1,6 +1,7 @@
 const chronometerEl = document.querySelector('.chronometer')
 const startEl = document.querySelector('#start')
 const stopEl = document.querySelector('#stop')
+const resetEl = document.querySelector('#reset')
 
 let second = 0
 let minute = 0
@@ -32,6 +33,13 @@ const stop = () => {
     clearInterval(time)
 }
 
+const reset = () => {
+    second = 0
+    minute = 0
+    hour = 0
+    renderChronometer()
+}
+
 
 renderChronometer()
 
@@ -41,4 +49,8 @@ startEl.addEventListener('click', () => {
 
 stopEl.addEventListener('click', () => {
     stop()
+})
+
+resetEl.addEventListener('click', () => {
+    reset()
 })
